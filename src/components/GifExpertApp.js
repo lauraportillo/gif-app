2169be06a3ab0dfdc56a5802ebb809f784bee185
Laura Portillo <lauraportillo.ark@gmail.com>
2022-04-06 14:10:39 +0200

    add categories state

--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -3,10 +3,25 @@ import '../stylesheets/GifExpertApp.scss';
 import '../stylesheets/Reset.scss';
 
 const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+
+
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr />
+
+      <button>Agregar</button>
+
+      <ol>
+        {
+          categories.map(category => {
+            return <li key={category}> {category}</li>
+          })
+        }
+      </ol>
     </>
   );
 }
