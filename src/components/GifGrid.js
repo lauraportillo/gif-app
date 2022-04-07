e02d985a3b69ede7f6a90d3145a4595952d304ea
Laura Portillo <lauraportillo.ark@gmail.com>
2022-04-07 13:58:45 +0200

    add fetch api gif grid component

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,11 +1,30 @@
 import '../stylesheets/GifGrid.scss';
 
 const GifGrid = ({ category }) => {
+
+    const getGifs = async () => {
+
+        const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=Pqt6FbLsz7CSsS6u9JjEmcoxwSF2tTiI';
+        const resp = await fetch(url);
+        const { data } = await resp.json();
+
+        const gifs = data.map(img => {
+            return {
+                id: img.id,
+                title: img.title,
+                url: img.images?.downsized_medium.url
+            }
+        })
+        console.log(gifs);
+    }
+
+    getGifs();
+
+
+
     return (
         <>
-
             <h3>{category}</h3>
-
         </>
     )
 }
