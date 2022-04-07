2850514b877fe691e4b07894a48cd46dd260cb38
Laura Portillo <lauraportillo.ark@gmail.com>
2022-04-07 14:09:34 +0200

    add useEffect

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,9 +1,16 @@
+import { useState, useEffect } from 'react';
 import '../stylesheets/GifGrid.scss';
 
 const GifGrid = ({ category }) => {
 
-    const getGifs = async () => {
+    const [count, setCount] = useState(0);
 
+    useEffect(() => {
+        getGifs();
+    }, []);
+
+
+    const getGifs = async () => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=Pqt6FbLsz7CSsS6u9JjEmcoxwSF2tTiI';
         const resp = await fetch(url);
         const { data } = await resp.json();
@@ -17,10 +24,6 @@ const GifGrid = ({ category }) => {
         })
         console.log(gifs);
     }
-
-    getGifs();
-
-
 
     return (
         <>
