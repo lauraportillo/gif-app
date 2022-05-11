209e88f31a61e50966579257c2e7765c59d31944
Laura Portillo <lauraportillo.ark@gmail.com>
2022-05-11 10:56:06 +0200

    add a test to evaluate image and paragraph

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -7,13 +7,26 @@ describe('Testing <GifGridItem />', () => {
 
     const title = 'One title';
     const url = 'https://localhost/anything.jpg';
+    const wrapper = shallow(<GifGridItem title={title} url={url} />);
 
     test('should show the component correctly', () => {
-
-        const wrapper = shallow(<GifGridItem title={title} url={url} />);
-
         expect(wrapper).toMatchSnapshot();
+    })
 
-    });
+    test('should have a paragraph with title', () => {
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+    })
 
-});
+    test('should have image equal to the url and alt of the props', () => {
+        const img = wrapper.find('img');
+        // console.log(img.html()); // para ver todo el objeto
+        // src y alt son properties para react:
+        // console.log(img.props());
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    })
+
+    test('should have the className animate__fadeIn', () => { })
+
+})
